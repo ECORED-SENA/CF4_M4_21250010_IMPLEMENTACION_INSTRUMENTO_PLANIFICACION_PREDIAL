40e7a2fc3bbccdae1b17733bf3cca18ae7de578b
Cielo Angulo <cielodamarisangulo@gmail.com>
2025-12-21 13:53:25 -0500

    update

--- src/config/global.js
+++ src/config/global.js
@@ -134,7 +134,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/dist.pdf',
+        download: 'downloads/21250010_CF04_DU.pdf',
       },
       {
         icono: 'fas fa-download',
